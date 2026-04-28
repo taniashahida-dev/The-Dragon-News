18b7c645a565e47c6e8de0da35a6435023906136
taniashahida-dev <taniashahida993@gmail.com>
2026-04-28 18:50:50 +0600

    Make login and registration form with react hook form

--- src/app/(auth)/loginpage/page.jsx
+++ src/app/(auth)/loginpage/page.jsx
@@ -1,27 +1,65 @@
+"use client"
 import Link from "next/link";
+import { useForm } from "react-hook-form";
 
 const LogIn = () => {
+
+     const {
+    register,
+    handleSubmit,
+      formState: { errors },
+   
+  } = useForm()
+  const handleLogin =(data)=>{
+console.log(data)
+  }
   return (
     <div className="hero bg-base-200 h-[80vh]">
-    
       <div className="bg-white p-10 rounded-lg ">
-          <h1 className="text-3xl font-semibold text-center text-mauve-800">Login Your Account</h1>
-          <div className="divider my-5"></div>
+        <h1 className="text-3xl font-semibold text-center text-mauve-800">
+          Login Your Account
+        </h1>
+        <div className="divider my-5"></div>
 
-        <form className="space-y-2">
-      
-        <fieldset className="fieldset ">
-          <legend className="fieldset-legend">Email Address</legend>
-          <input type="email" className="input" placeholder="Enter Your Email" />
-        </fieldset>
-        <fieldset className="fieldset ">
-          <legend className="fieldset-legend">Password</legend>
-          <input type="password" className="input" placeholder="Enter Your Password" />
-        </fieldset>
-        <button className="btn bg-mauve-600 text-white w-full my-2">Login</button>
-       
-      </form>
-       <p className="text-center text-mauve-400 text-sm">Dont't Have An Account ?<Link href={'/registration'} className="text-purple-600"> Register</Link>  </p>
+        <form className="space-y-2" onSubmit={handleSubmit(handleLogin)}>
+          <fieldset className="fieldset ">
+            <legend className="fieldset-legend">Email Address</legend>
+            <input
+              type="email"
+              className="input"
+              placeholder="Enter Your Email"
+               {...register("email", { required:"Email field is required" })} 
+            />
+          </fieldset>
+
+{
+    errors.email && (<p className="text-xs text-red-600">{errors.email.message}</p>)
+}
+
+          <fieldset className="fieldset ">
+            <legend className="fieldset-legend">Password</legend>
+            <input
+              type="password"
+              className="input"
+              placeholder="Enter Your Password"
+     {...register("password", { required:"Password field is required" })} 
+            />
+          </fieldset>
+          {
+    errors.password && (<p className="text-xs text-red-600">{errors.password.message}</p>)
+}
+
+          <button className="btn bg-mauve-600 text-white w-full my-2">
+            Login
+          </button>
+        </form>
+        <p className="text-center text-mauve-400 text-sm">
+          Dont't Have An Account ?
+          <Link href={"/registration"} className="text-purple-600">
+            {" "}
+            Register
+          </Link>{" "}
+        </p>
       </div>
     </div>
   );
--- src/app/(auth)/registration/page.jsx
+++ src/app/(auth)/registration/page.jsx
@@ -1,11 +1,92 @@
 
+  "use client"
 
-const ReistrationPage = () => {
+  import { useForm } from "react-hook-form";
+  
+  const ReistrationPage = () => {
+  
+       const {
+      register,
+      handleSubmit,
+        formState: { errors },
+     
+    } = useForm()
+    const handleReg =(data)=>{
+  console.log(data)
+    }
     return (
-        <div>
-            registration page
+      <div className="hero bg-base-200 h-screen">
+        <div className="bg-white p-10 rounded-lg ">
+          <h1 className="text-3xl font-semibold text-center text-mauve-800">
+         Register your account
+          </h1>
+          <div className="divider my-5"></div>
+  
+          <form className="space-y-2" onSubmit={handleSubmit(handleReg)}>
+            <fieldset className="fieldset ">
+              <legend className="fieldset-legend">Your Name</legend>
+              <input
+                type="text"
+                className="input"
+                placeholder="Enter Your Name"
+                 {...register("name", { required:"Name field is required" })} 
+              />
+            </fieldset>
+  
+  {
+      errors.name && (<p className="text-xs text-red-600">{errors.name.message}</p>)
+  }
+            <fieldset className="fieldset ">
+              <legend className="fieldset-legend">Photo URL</legend>
+              <input
+                type="url"
+                className="input"
+                placeholder="Enter URL"
+                 {...register("photo", { required:"This field is required" })} 
+              />
+            </fieldset>
+  
+  {
+      errors.photo && (<p className="text-xs text-red-600">{errors.photo.message}</p>)
+  }
+            <fieldset className="fieldset ">
+              <legend className="fieldset-legend">Email Address</legend>
+              <input
+                type="email"
+                className="input"
+                placeholder="Enter Your Email"
+                 {...register("email", { required:"Email field is required" })} 
+              />
+            </fieldset>
+  
+  {
+      errors.email && (<p className="text-xs text-red-600">{errors.email.message}</p>)
+  }
+  
+            <fieldset className="fieldset ">
+              <legend className="fieldset-legend">Password</legend>
+              <input
+                type="password"
+                className="input"
+                placeholder="Enter Your Password"
+       {...register("password", { required:"Password field is required" })} 
+              />
+            </fieldset>
+            {
+      errors.password && (<p className="text-xs text-red-600">{errors.password.message}</p>)
+  }
+  
+            <button className="btn bg-mauve-600 text-white w-full my-2">
+              Register
+            </button>
+          </form>
+        
         </div>
+      </div>
     );
-};
+  };
+  
+  export default ReistrationPage;
+  
 
-export default ReistrationPage;
+
