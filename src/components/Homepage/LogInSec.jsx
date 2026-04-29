b830856063f26de03287ec35ce1566cebce2dfd6
taniashahida-dev <taniashahida993@gmail.com>
2026-04-29 13:14:41 +0600

    authentication with google and github

--- src/components/Homepage/LogInSec.jsx
+++ src/components/Homepage/LogInSec.jsx
@@ -1,17 +1,28 @@
+'use client'
+import { authClient } from "@/lib/auth-client";
 import { FaGithub, FaGoogle } from "react-icons/fa";
 
 
 const LogInSec = () => {
+    const googlesignIn = async () => {
+  const data = await authClient.signIn.social({
+    provider: "google",
+  });}
+    const githubsignIn = async () => {
+  const data = await authClient.signIn.social({
+    provider: "github",
+  });
+};
     return (
         <div>
              <h1 className="text-xl font-semibold mb-3">Login With</h1>
              <div className="space-y-3">
                
 
-                  <button className="flex gap-2 items-center btn w-full text-lg text-blue-500 border-blue-500"><FaGoogle />
+                  <button className="flex gap-2 items-center btn w-full text-lg text-blue-500 border-blue-500" onClick={googlesignIn}><FaGoogle />
                     Login With Google</button>
                 
-                    <button className="flex gap-2 items-center btn w-full text-lg border-black">
+                    <button className="flex gap-2 items-center btn w-full text-lg border-black" onClick={githubsignIn}>
                        <FaGithub /> Login With Github</button>
                 
              </div>
